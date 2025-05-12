e16803ae287b6fc30e63b2985a53b275830da802
paraIncog <117535473+paraIncog@users.noreply.github.com>
2025-05-12 03:33:09 +0300

    init gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/err-tryout-web/",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
